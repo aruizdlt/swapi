fcc84dc32d9f66d9b08034b655a5c33cd3c6acd9
Antonio Ruiz de la Torre <aruizdlt@gmail.com>
2024-11-02 11:37:54 +0100

    test(locust): performance test

---
+++ tests/locustfile.py
@@ -0,0 +1,13 @@
+from locust import HttpUser, task, constant
+
+class DemoUser(HttpUser):
+    # Simula el comportamiento de los usuarios con un tiempo de espera entre tareas
+    wait_time = constant(1)
+
+    @task
+    def get_all_people(self):
+        self.client.get("/people")
+
+    @task
+    def get_sorted_people(self):
+        self.client.get("/people/names-sorted")
